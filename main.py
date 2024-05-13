57ab42390960743d8e229eaeee339ea7b613a4e8
saragh21 <114303824+saragh21@users.noreply.github.com>
2024-05-13 15:37:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from PyQt6.QtWidgets import QApplication, QWidget,QMainWindow
+ from PyQt6.QtWidgets import QApplication, QWidget,QMainWindow
 from PianoWidget1 import PianoWidget1
 from Piano_graphic_user_interface import piano_GUI
-
+ 
 
 if __name__ == '__main__':
     import sys
     global app # Use global to prevent crashing on exit
     app = QApplication(sys.argv)
-    # piano = PianoWidget1()# an instance from pianowidet
-    # MainWindow =QMainWindow()
-    # piano.setupUi(MainWindow)
-    # #Gui.draw_keys(MainWindow)
-    gui=piano_GUI(500)
-    #gui.init_window()
+    gui=piano_GUI(400)
     gui.show()
-    #Gui.play_sound('h4')
-    #Gui.play_sound('d4')
     sys.exit(app.exec())
  
      
